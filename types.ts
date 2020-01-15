7297884b6c18b44f386eba0e8ec69a4d83c6431e
Chandrakant <chandrakantsavvy@gmail.com>
2020-01-15 08:01:04 +0530

    learned data types

---
+++ types.ts
@@ -0,0 +1,23 @@
+// primitives
+var a: number;
+var b: boolean;
+var c: string;
+
+// not really useful
+var d: undefined;
+var e: null;
+
+// array types
+var f: number[];
+var g: string[];
+
+// tuples
+var h: [number, boolean];
+var i: [string, number];
+
+// any type
+var j: any;
+
+// number or boolean type => union type
+var k: number | boolean;
+var l: number | boolean | string;
