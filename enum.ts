49472d113b600e6a84c4f2669af9582392e5aa1b
Chandrakant <chandrakantsavvy@gmail.com>
2020-01-15 08:07:45 +0530

    learned about enums

---
+++ enum.ts
@@ -0,0 +1,5 @@
+enum DayOfWeek {
+  SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
+
+console.log(DayOfWeek.FRIDAY); // Outputs: 5, index of Friday. beginning from zero at SUNDAY.
